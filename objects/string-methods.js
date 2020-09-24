efc34f532e96435c592b1e1f1bc044e13da87f33
DeAndre Boston <dboston3@yahoo.com>
2020-09-23 21:50:03 -0500

    Practice string methods

--- objects/string-methods.js
+++ objects/string-methods.js
@@ -1,4 +1,33 @@
-let name = 'DeAndre'
+let name = ' DeAndre '
 
 //length Property
 console.log(name.length)
+
+//convert to upper case
+ console.log(name.toUpperCase())
+
+// convert to lowrer case
+console.log(name.toLowerCase())
+
+//inludes method
+let password = 'abs123pasnsword098'
+console.log(password.includes('password'))
+
+//trim method - remove extra white space
+console.log(name.trim())
+
+//challenge
+
+//isValidPassword
+//length is more than 8 - and does not contain word assword
+
+let isValidPassword = function (password) {
+   return password.length > 8 && !password.includes('password')
+
+}
+
+
+
+console.log(isValidPassword('asdfasdf'))
+console.log(isValidPassword('dfasdfspassword'))
+console.log(isValidPassword('qazzwsxedcr'))
