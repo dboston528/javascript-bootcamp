c2f50e585f4fbb537cba23c5b7e1bf943df8a369
DeAndre Boston <dboston3@yahoo.com>
2020-09-28 13:29:31 -0500

    Playing with var DO NOT USE VAR

---
+++ objects/var.js
@@ -0,0 +1,16 @@
+age = 10
+console.log(age)  
+var age
+
+
+/*
+var lets you redeclare a variable using the same name. Using let and const
+the progrma will crash if you try to assign a varible using the same name
+
+var uses function scoped, not block scope.
+
+const and let use block scope
+
+var gets hoisted to the top of the scope as a declaration. 
+
+ */
