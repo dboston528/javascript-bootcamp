f1ef0cc964054d8d2b1436b8b9e2b0ce033af606
DeAndre Boston <dboston3@yahoo.com>
2020-09-30 15:22:48 -0500

    Add updates on array manipulation

---
+++ arrays/notes.js
@@ -0,0 +1,16 @@
+
+const notes = ['Note 1', 'Note 2', 'Note 3']
+
+// console.log(notes.pop())
+// notes.push('My new note')
+
+// console.log(notes.shift())
+// notes.unshift('My first note')
+
+// notes.splice(1,0, 'This is the new seond item')
+
+notes[2] = 'This is the new note 3'
+
+
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
+console.log(notes)
---
+++ arrays/todo.js
@@ -0,0 +1,12 @@
+const todo =  ['Create video', 'Cook breakfast', 'Walk dog', 'Take shower', 'Wash clothes']
+
+// Delete the third item
+// Add a new item to the end
+// remove the first item the list
+
+todo.splice(2,1)
+todo.push('Update website')
+todo.shift();
+
+console.log(`You have ${todo.length} todos!`)
+console.log(todo)
