10fad20bbd469090ca47a646b8a16ea3551295ee
DeAndre Boston <dboston3@yahoo.com>
2020-10-16 12:55:33 -0500

    Update comments in const.js

--- objects/const.js
+++ objects/const.js
@@ -1,11 +1,12 @@
 const person = {age: 27}
 
-// person = {} would be an invalif statement becaue but can still manipulate values int the alread exesitig object
+// person = {} would be an invalid statement, but you can still manipulate values in the already exesitig object.
 person.age = 28
 
 console.log(person)
 
 /* NOTES:
 The differnce between a let based variable and a const based 
-variable is that it can't be reassinged after it has been created.
+variable is that a const based variable can't be reassinged 
+after it has been created.
 */
